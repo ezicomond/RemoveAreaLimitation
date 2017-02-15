d4783187891d843876093bf79a724959514cca70
DejaVu <chenckluo@hotmail.com>
2017-02-15 15:18:41 +0800

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace RemoveAreaLimit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerDetect = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timerDetect
+            // 
+            this.timerDetect.Enabled = true;
+            this.timerDetect.Interval = 1000;
+            this.timerDetect.Tick += new System.EventHandler(this.timerDetect_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "汉化过程结束后运行此程序";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "程序会自动检测游戏进程是否存在";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "检测到游戏进程后会自动退出";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(85, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(317, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "(若未自动退出，则请勿在游戏进行时退出，以免断开连接)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(342, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "By Ezicomond";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 319);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "去除区域限制";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerDetect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+using Fiddler;
+
+namespace RemoveAreaLimit
+{
+    public partial class MainForm : Form
+    {
+        private const string MATCH_URL = "http://auth.black.game.daum.net/api/starter/black/launch.json";
+        private const string NOT_ABALIABLE = "NOT_AVAILABLE_COUNTRY";
+        private const string RDT_URL = "http://black.game.daum.net/black/index.daum";
+
+        private int port = 5973;
+
+        static string sSecureEndpointHostname = "localhost";
+        static int iSecureEndpointPort = 3965;
+
+        static Proxy oSecureEndpoint;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            FiddlerApplication.SetAppDisplayName("RemoveAreaLimit");
+            FiddlerCoreStartupFlags oFCSF = FiddlerCoreStartupFlags.Default;
+
+            FiddlerApplication.Startup(port, oFCSF);
+
+            oSecureEndpoint = FiddlerApplication.CreateProxyEndpoint(iSecureEndpointPort, true, sSecureEndpointHostname);
+
+            FiddlerApplication.BeforeRequest += OnBeforeRequest;
+            FiddlerApplication.BeforeResponse += OnBeforeResponse;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FiddlerApplication.Shutdown();
+        }
+
+        void OnBeforeRequest(Session session)
+        {
+            session.utilDecodeRequest();
+
+            if (session.fullUrl.IndexOf(MATCH_URL) != -1)
+                session.bBufferResponse = true;
+        }
+
+        void OnBeforeResponse(Session session)
+        {
+            session.utilDecodeResponse();
+
+            if (session.fullUrl.IndexOf(MATCH_URL) != -1)
+            {
+                string response = session.GetResponseBodyAsString();
+
+                int codeLoc = response.IndexOf(NOT_ABALIABLE);
+                if (codeLoc != -1)
+                {
+                    response = response.Substring(0, codeLoc) + "PASS" +
+                               response.Substring(codeLoc + NOT_ABALIABLE.Length,
+                                   response.Length - codeLoc - NOT_ABALIABLE.Length);
+
+                    int msgCtlLoc = response.IndexOf(RDT_URL);
+                    response = response.Substring(0, msgCtlLoc) +
+                               response.Substring(msgCtlLoc + RDT_URL.Length,
+                                   response.Length - msgCtlLoc - RDT_URL.Length);
+
+                    session.utilSetResponseBody(response);
+                }
+
+            }
+
+        }
+
+        bool ProcessExists(string PName)
+        {
+            Process[] proc = Process.GetProcessesByName(PName);
+
+            return proc.Length != 0;
+        }
+
+        private void timerDetect_Tick(object sender, EventArgs e)
+        {
+            if (ProcessExists("BlackDesert_Launcher"))
+            {
+                this.Close();
+            }
+        }
+    }
+}
